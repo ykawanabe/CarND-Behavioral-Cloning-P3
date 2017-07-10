d087e4985783680fdfa38dcd377a2e2d6fec5f0a
yusuke-kawanabe <ysk.kwnb.915@gmail.com>
2017-07-09 17:52:48 -0700

    Update the model

--- model.py
+++ model.py
@@ -27,10 +27,10 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Flatten(input_shape=(1,160,320,3)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
 
 model.save('model.h5')
