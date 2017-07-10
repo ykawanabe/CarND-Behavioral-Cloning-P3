457cfaf7fac1babf6111ceedc5d8ef42d96e20dd
yusuke-kawanabe <ysk.kwnb.915@gmail.com>
2017-07-09 17:39:46 -0700

    Fix append failing

--- model.py
+++ model.py
@@ -8,14 +8,14 @@ with open('../data/driving_log.csv') as csvfile:
     for line in reader:
         lines.append(line)
 
-image = []
+images = []
 measurements = []
 for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
     current_path = '../data/IMG' + filename
     image = cv2.imread(current_path)
-    image.append(image)
+    images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
 
