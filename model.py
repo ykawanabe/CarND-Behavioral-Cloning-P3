de9ca5dd9a8ea3e63c5cf7dc0075bfc8f00c428b
yusuke-kawanabe <ysk.kwnb.915@gmail.com>
2017-07-13 08:46:48 -0700

    Add more data

--- model.py
+++ model.py
@@ -12,20 +12,21 @@ images = []
 measurements = []
 for line in lines:
     for i in range(3):
-        source_path = line[i]
-        filename = source_path.split('/')[-1]
-        current_path = '../data/IMG/' + filename
-        image = cv2.imread(current_path)
-        images.append(image)
-        measurement = float(line[3])
-        correction = 0.2
-        if i == 1:
-            measurement + correction
-        elif i == 2:
-            measurement - correction
-        measurements.append(measurement)
-        images.append(cv2.flip(image,1))
-        measurements.append(measurement*-1.0)
+        for j in range(2):
+            source_path = line[i]
+            filename = source_path.split('/')[-1]
+            current_path = '../data/IMG/' + filename
+            image = cv2.imread(current_path)
+            images.append(image)
+            measurement = float(line[3])
+            correction = 0.2
+            if i == 1:
+                measurement + correction
+            elif i == 2:
+                measurement - correction
+            measurements.append(measurement)
+            images.append(cv2.flip(image,1))
+            measurements.append(measurement *-1.0)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
@@ -36,11 +37,13 @@ from keras.layers import Lambda
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers import Cropping2D
+# from keras.models import Model
+# from matplotlib.pyplot as plt
 
 model = Sequential()
 model.add(Cropping2D(cropping=((50,20),(0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(24,5,5,subsamddple=(2,2),activation="relu"))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(64,3,3,activation="relu"))
