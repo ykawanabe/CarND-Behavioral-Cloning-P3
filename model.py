7d40f762434c033fdaafb2d2a284bfe95df59934
yusuke-kawanabe <ysk.kwnb.915@gmail.com>
2017-07-14 01:39:14 -0700

    Fix bugs

--- model.py
+++ model.py
@@ -2,11 +2,25 @@ import csv
 import cv2
 import shutil
 
+files = []
+for i in range(1):
+    files.append([])
+    with open('../data%s/driving_log.csv' % str(i)) as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            files[i].append(line)
+
 samples = []
-with open('../data2/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
+for idx, lines in enumerate(files):
+    for line in lines:
         samples.append(line)
+        for i in range(3):
+            source_path = line[i]
+            filename = source_path.split('/')[-1]
+            current_path = '../data' + str(idx) + '/IMG/' + filename
+            new_path = '../IMG/' + filename
+            shutil.copy2(current_path, new_path)
+
 
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
@@ -14,7 +28,7 @@ train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 import numpy as np
 import sklearn
 
-def generator(samples, batch_size=32768):
+def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1:
         for offset in range(0, num_samples, batch_size):
@@ -23,20 +37,28 @@ def generator(samples, batch_size=32768):
             images = []
             measurements = []
             for line in batch_samples:
-                source_path = line[i]
-                filename = source_path.split('/')[-1]
-                current_path = '../data2/IMG/' + filename
-                image = cv2.imread(current_path)
-                images.append(image)
-                measurement = float(line[3])
-                measurements.append(measurement)
+                for i in range(3):
+                    source_path = line[i]
+                    filename = source_path.split('/')[-1]
+                    current_path = '../IMG/' + filename
+                    image = cv2.imread(current_path)
+                    images.append(image)
+                    measurement = float(line[3])
+                    correction = 0.2
+                    if i == 1:
+                        measurement + correction
+                    elif i == 2:
+                        measurement - correction
+                    measurements.append(measurement)
+                    images.append(cv2.flip(image,1))
+                    measurements.append(measurement *-1.0)
             X_train = np.array(images)
             y_train = np.array(measurements)
             yield sklearn.utils.shuffle(X_train, y_train)
 
 
-train_generator = generator(train_samples, batch_size=32768)
-validation_generator = generator(validation_samples, batch_size=32768)
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense
@@ -63,5 +85,6 @@ model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 # model.fit(X_train, y_train, verbose=1, validation_split=0.2, shuffle=True, epochs=4)
-model.fit_generator(train_generator, steps_per_epoch=len(train_samples), validation_data=validation_generator, validation_steps=(validation_samples), epochs=1)
+model.fit_generator(train_generator, steps_per_epoch=6*len(train_samples), validation_data=validation_generator, validation_steps=6*(validation_samples), epochs=3)
+
 model.save('model.h5')
